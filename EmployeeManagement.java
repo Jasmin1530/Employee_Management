5577fef8ab3849a596dca6ff000c12486c306fdf
Jasmin Jennifer <jasmin.jennifer@bcah.christuniversity.in>
2025-03-25 19:50:23 +0530

    EmployeeManagement.java

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -70,7 +70,7 @@ public class EmployeeManagement extends JFrame implements ActionListener {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_db", "root", "02_jasmin");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_db", "root", " ");
             System.out.println("Database Connected Successfully!");
         } catch (Exception e) {
             System.out.println("Database Connection Error: " + e.getMessage());
